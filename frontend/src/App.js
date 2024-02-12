d69fb121455f7e45647de2114923b198db675cd1
groom2hub <groomwnsgur35@gmail.com>
2024-02-12 23:13:24 +0900

    Feat: React+FastAPI - Backend & Frontend 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [data1, setData1] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000')  // FastAPI 서버 주소
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/hi')
+      .then(response => response.json())
+      .then(data1 => setData1(data1));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>FastAPI and React.js</h1>
+      {data && <p>{data.Hello}</p>}
+      {data1 && <p>{data1.Hi}</p>}
     </div>
   );
 }
