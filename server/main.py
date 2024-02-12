cfe7a53bcb575141bd2f63f50dce0f826fb54f55
groom2hub <groomwnsgur35@gmail.com>
2024-02-13 00:14:11 +0900

    Rename: backend -> server

---
+++ server/main.py
@@ -0,0 +1,26 @@
+# backend/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # React 앱이 실행 중인 포트
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Hello World"}
+
+@app.get("/hi")
+def read_root1():
+    return {"Hi": "Hi World"}
