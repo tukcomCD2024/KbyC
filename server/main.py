334eb88b0871d0c381d48cc75fe73946d1667774
Blu2ming <yeox84@naver.com>
2024-02-14 02:14:30 +0900

    데이터베이스 생성

--- server/main.py
+++ server/main.py
@@ -24,3 +24,19 @@ def read_root():
 @app.get("/hi")
 def read_root1():
     return {"Hi": "Hi World"}
+
+
+# 테이블 생성
+import models
+from database import engine
+models.Base.metadata.create_all(bind=engine)
+
+# 테이블 데이터 추가
+from models import User
+from datetime import datetime
+new_user = User(user_name="test", user_password="1234", user_email="test@naver.com", user_status="회원", created_at=datetime.now(), last_connected_at=datetime.now())
+from database import SessionLocal
+db = SessionLocal()
+db.add(new_user)
+db.commit()
+db.close()
---
+++ server/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float
+
+from database import Base
+
+class User(Base):
+    __tablename__ = 'user'
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    user_name = Column(String(50), nullable=False)
+    user_password = Column(String(100), nullable=False)
+    user_email = Column(String(50), unique=True, nullable=False)
+    user_status = Column(String(50), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    last_connected_at = Column(DateTime, nullable=False)
+
+class SearchHistory(Base):
+    __tablename__ = 'searchHistory'
+
+    search_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    user_id = Column(Integer, nullable=False)
+    keyword_id = Column(Integer, nullable=False)
+    search_date = Column(DateTime, nullable=False)
+
+class Keyword(Base):
+    __tablename__ = 'keyword'
+
+    keyword_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    keyword_name = Column(String(50), nullable=False)
+    speak_sum = Column(Integer, nullable=False)
+    age_rate = Column(Float, nullable=False)
+    positive_rate = Column(Float, nullable=False)
+
+class Community(Base):
+    __tablename__ = 'community'
+
+    community_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    keyword_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, nullable=False)
+    content = Column(Text, nullable=False)
+    write_date = Column(DateTime, nullable=False)
+    report_sum = Column(Integer, nullable=False)
