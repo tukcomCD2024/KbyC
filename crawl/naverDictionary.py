3989aa9c0132527b3562d61426a6ebe766ea663f
groom2hub <groomwnsgur35@gmail.com>
2024-02-20 19:38:35 +0900

    Feat: Naver Dictionary crawling result list

--- crawl/naverDictionary.py
+++ crawl/naverDictionary.py
@@ -3,6 +3,9 @@ import os
 import sys
 import urllib.request
 import json
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+import re
 
 # secret.json에서 api key 가져오기
 with open('./secret.json', 'r', encoding='utf8') as f:
@@ -11,23 +14,21 @@ with open('./secret.json', 'r', encoding='utf8') as f:
     client_secret = data["naver_dic_client_secret"]
 
 # 검색할 단어
-encText = urllib.parse.quote("virus")
-url = "https://openapi.naver.com/v1/search/encyc.xml?query=" + encText + "&display=10&start=1&sort=sim" # JSON 결과
+input_text = "virus"
+enc_text = urllib.parse.quote(input_text)
+url = "https://openapi.naver.com/v1/search/encyc.xml?query=" + enc_text + "&display=10&start=1&sort=sim" # JSON 결과
 # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
 request = urllib.request.Request(url)
 request.add_header("X-Naver-Client-Id",client_id)
 request.add_header("X-Naver-Client-Secret",client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
+response = urlopen(request).read()
+soup = bs(response, "xml")
 
-print("""
-#####################################################################################
-        naverDictionary 결과값
-#####################################################################################
-""")
-
-if(rescode==200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
+response_list=[]
+for item in soup.find_all('item') :
+    if item:
+        description_text = re.sub(re.compile(r'<.*?>'), '', item.find("description").text)
+        response_list.append({
+                 "요약": description_text,
+                 'Link':item.find("link").text})
+print(response_list)
