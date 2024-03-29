731c140f967cd2e89041c8ead6105add4be0a417
Blu2ming <yeox84@naver.com>
2024-03-29 20:36:19 +0900

    Fix: User Information Update

--- server/user/user_crud.py
+++ server/user/user_crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from models import User
-from user.user_schema import UserCreate, UserUpdate, UsernameUpdate, PasswordUpdate
+from user.user_schema import UserCreate, UsernameUpdate, PasswordUpdate
 import datetime
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
@@ -50,24 +50,6 @@ def read_my_info(db: Session, email: str):
     if not user:
         raise HTTPException(status_code=404, detail="User Not Found")
     
-    return user
-
-def update_user(db: Session, user: UserUpdate, email: str):
-    update = db.query(User).filter(User.user_email == email).first()
-
-    if not update:
-        raise HTTPException(status_code=404, detail="User Not Found")
-    
-    u = db.query(User).filter(User.user_name == user.username).first()
-
-    if u:
-        raise HTTPException(status_code=409, detail="Username Already Exists")
-
-    update.user_name = user.username
-    update.user_password = pwd_context.hash(user.password)
-
-    db.commit()
-
     return user
 
 def update_username(db: Session, user: UsernameUpdate, email: str):
--- server/user/user_schema.py
+++ server/user/user_schema.py
@@ -20,16 +20,6 @@ class UserCreate(BaseModel):
         if 'password' in values.data and v != values.data['password']:
             raise HTTPException(status_code=422, detail="비밀번호가 일치하지 않습니다.")
         return v
-    
-class UserUpdate(BaseModel):
-    username: str
-    password: str
-
-    @field_validator('username', 'password')
-    def check_empty(cls, v):
-        if not v or v.isspace():
-            raise HTTPException(status_code=422, detail="필수 항목을 입력해주세요.")
-        return v
 
 class UsernameUpdate(BaseModel):
     username: str
