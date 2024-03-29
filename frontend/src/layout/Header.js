66df884084c1e061a006d34180c740ae73143047
groom2hub <groomwnsgur35@gmail.com>
2024-03-29 21:09:27 +0900

    Desing: apply Header

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../App.css'
 
 const Header = () => {
     return (
         <header>
-            <Link to={'/'}><button>홈</button></Link>
+            <Link to={'/'}><button className='App-header-link'>홈</button></Link>
             &nbsp;&nbsp;
-            <Link to={'/board'}><button>게시판</button></Link>
+            <Link to={'/board'}><button className='App-header-link'>게시판</button></Link>
             &nbsp;&nbsp;
-            <Link to={'/service/chatgpt'}><button>ChatGPT</button></Link>
+            <Link to={'/service/chatgpt'}><button className='App-header-link'>ChatGPT</button></Link>
             &nbsp;&nbsp;
-            <Link to={'/service/navernews'}><button>네이버 뉴스</button></Link>
+            <Link to={'/service/navernews'}><button className='App-header-link'>네이버 뉴스</button></Link>
             <hr/>
         </header>
     );
