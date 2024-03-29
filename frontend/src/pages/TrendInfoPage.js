7a4ee796118fdd674b26d283ad15b130f505b014
groom2hub <groomwnsgur35@gmail.com>
2024-03-29 14:29:48 +0900

    Design: add Trendinfo content

--- frontend/src/pages/TrendInfoPage.js
+++ frontend/src/pages/TrendInfoPage.js
@@ -15,6 +15,17 @@ const TrendInfoPage = () => {
           </div>
         </div>
       </div>
+
+      <div className='trendinfo-content-container'>
+          <div className='trendinfo-content'>
+            <p className='trendinfo-content-title'>정의</p>
+            <p className='trendinfo-content-list'>정의1</p>
+            <p className='trendinfo-content-title'>관련 기사</p>
+            <p className='trendinfo-content-list'>관련 기사1</p>
+            <p className='trendinfo-content-title'>반응</p>
+            <p className='trendinfo-content-list'>반응1</p>
+          </div>
+        </div>
     </div>
   );
 }
