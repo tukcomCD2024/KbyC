e6fb8e60f343a2d73a5f18828662a6000883d67d
groom2hub <groomwnsgur35@gmail.com>
2024-03-29 14:27:53 +0900

    Design: Trendinfo.js

---
+++ frontend/src/pages/TrendInfoPage.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import './TrendInfoPage.css';
+
+const TrendInfoPage = () => {
+
+  return (
+    <div className='trendinfo-page'>
+
+    </div>
+  );
+}
+
+export default TrendInfoPage;
