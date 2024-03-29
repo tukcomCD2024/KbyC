fe50ec9d10b0696d021a9b329918ba77907f0715
groom2hub <groomwnsgur35@gmail.com>
2024-03-29 14:28:57 +0900

    Design: add Trendinfo sidebar

--- frontend/src/pages/TrendInfoPage.js
+++ frontend/src/pages/TrendInfoPage.js
@@ -5,7 +5,16 @@ const TrendInfoPage = () => {
 
   return (
     <div className='trendinfo-page'>
-
+      <div className='trendinfo-content-wrapper'>
+        <div className='trendinfo-sidebar-container'>
+          <div className='trendinfo-sidebar'>
+            <p className='trendinfo-sidebar-title'>Trend</p>
+            <p className='trendinfo-sidebar-list'>정의</p>
+            <p className='trendinfo-sidebar-list'>관련 기사</p>
+            <p className='trendinfo-sidebar-list'>반응</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
