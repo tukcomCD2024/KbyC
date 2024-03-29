cbfcaa6fb3cce946a95ecd2c8a906b67d8219f3e
Blu2ming <yeox84@naver.com>
2024-03-29 22:53:07 +0900

    Feat: Google Trends

---
+++ server/service/google_trends.py
@@ -0,0 +1,25 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+
+# https://codereview.stackexchange.com/questions/208277/web-scraping-google-trends-in-python
+def fetch_xml(country_code):
+    url = f"https://trends.google.com/trends/trendingsearches/daily/rss?geo={country_code}"
+    start = time.time()
+    response = requests.get(url)
+    response_time = time.time() - start
+    print(f"The request took {response_time}s to complete.")
+    return response.content
+
+def trends_retriever(country_code):
+    xml_document = fetch_xml(country_code)
+    soup = BeautifulSoup(xml_document, "lxml")
+    titles = soup.find_all("title")[1:]
+    approximate_traffic = soup.find_all("ht:approx_traffic")
+    links = soup.find_all("link")
+    return {title.text: traffic.text
+            for title, traffic in zip(titles, approximate_traffic)}
+
+
+trends = trends_retriever("KR")
+print(trends)
