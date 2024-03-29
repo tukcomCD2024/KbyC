e0d9ab3f11aa4508b16a76e4bf48ce40e4cb4932
groom2hub <groomwnsgur35@gmail.com>
2024-03-29 21:12:04 +0900

    Design: add Logo img

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css'
+import logo from '../imgs/Treddit_logo_1.png'
 
 const Header = () => {
     const handleNavigation = (path) => {
@@ -8,7 +9,9 @@ const Header = () => {
 
     return (
         <header className='App-header'>
-            <button className='App-header-link-logo' onClick={() => handleNavigation('/')}>홈</button>
+            <button className='App-header-link-logo' onClick={() => handleNavigation('/')}>
+                <img src={logo} alt='home'className='App-header-link-logo-image' />
+            </button>
             <button className='App-header-link' onClick={() => handleNavigation('/board')}>게시판</button>
             <button className='App-header-link' onClick={() => handleNavigation('/service/chatgpt')}>ChatGPT</button>
             <button className='App-header-link' onClick={() => handleNavigation('/service/navernews')}>네이버 뉴스</button>
