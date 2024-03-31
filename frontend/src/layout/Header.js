04f2edf8f943da234e03336f80b144277547db0e
Blu2ming <yeox84@naver.com>
2024-03-31 16:24:32 +0900

    Feat: Google Trends

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -11,6 +11,8 @@ const Header = () => {
             <Link to={'/service/chatgpt'}><button>ChatGPT</button></Link>
             &nbsp;&nbsp;
             <Link to={'/service/navernews'}><button>네이버 뉴스</button></Link>
+            &nbsp;&nbsp;
+            <Link to={'/service/googletrends'}><button>구글 트렌드</button></Link>
             <hr/>
         </header>
     );
--- frontend/src/pages/GoogleTrends.js
+++ frontend/src/pages/GoogleTrends.js
@@ -5,12 +5,14 @@ axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 const GoogleTrends = () => {
     const [trends, setTrends] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function getGoogleTrends() {
             try {
                 const response = await axios.get('/service/googletrends');
                 setTrends(response.data.google_trends);
+                setLoading(false);
             }
             catch (error) {
                 console.error('에러 발생', error);
@@ -24,6 +26,7 @@ const GoogleTrends = () => {
             <h1>
                 구글 트렌드
             </h1>
+            {loading && <div>로딩 중...</div>}
             {trends.map((trend, index) => (
                 <div key={index}>
                     {index + 1}. {trend.title}<br/>
