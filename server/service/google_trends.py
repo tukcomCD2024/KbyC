647c8643c6c87eade3cd7aaeb40ac976ca6e97a3
Blu2ming <yeox84@naver.com>
2024-04-01 03:29:29 +0900

    Feat: Google Trends

--- server/service/google_trends.py
+++ server/service/google_trends.py
@@ -19,7 +19,12 @@ def trends_retriever(country_code):
         traffic = item.find("ht:approx_traffic")
         news_urls = item.find_all("ht:news_item_url")
         news_urls_text = [url.text for url in news_urls]
-        trend = {"title": title.text, "traffic": traffic.text, "news_urls": news_urls_text}
+        news_titles = item.find_all("ht:news_item_title")
+        news_titles_text = [title.text for title in news_titles]
+        #trend = {"title": title.text, "traffic": traffic.text, "news_urls": news_urls_text, "news_titles": news_titles_text}
+        #trends.append(trend)
+        trend = {"title": title.text, "traffic": traffic.text,
+                 "news_list": [{"news_title": news_title, "news_url": news_url} for news_title, news_url in zip(news_titles_text, news_urls_text)]}
         trends.append(trend)
     return trends
 
