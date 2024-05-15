74ff4f47eb9a410389898bbb7f272f4ad284f00f
groom2hub <groomwnsgur35@gmail.com>
2024-05-15 21:05:14 +0900

    Feat: naver article' sa_text

--- crawl/naverArticleCrawl.py
+++ crawl/naverArticleCrawl.py
@@ -8,3 +8,12 @@ response = requests.get(url)
 if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
+
+    # sa_text 클래스를 가진 모든 요소를 가져옵니다.
+    sa_texts = soup.find_all('div', class_='sa_text')
+    
+    # 데이터를 담을 리스트 초기화
+    data = []
+    
+    for sa_text in sa_texts:
+        print(sa_text)
