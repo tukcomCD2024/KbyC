26533099e7176d8238b1d18f6e0f4a69fb4ec52d
groom2hub <groomwnsgur35@gmail.com>
2024-05-16 06:13:27 +0900

    Feat: top TF-IDF word extraction function

--- crawl/naverArticleNPL.py
+++ crawl/naverArticleNPL.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from konlpy.tag import Komoran
+import time
 
 csv_filename = "./100_264.csv"
 df = pd.read_csv(csv_filename)
@@ -27,3 +28,13 @@ def get_top_tfidf_words(tfidf_matrix, feature_names, top_n=10):
         top_word_indices = row.argsort()[-top_n:][::-1]
         top_words.append([feature_names[i] for i in top_word_indices])
     return top_words
+
+top_words = get_top_tfidf_words(tfidf_matrix=tfidfv.transform(morphs_list).toarray(), feature_names=tfidfv.get_feature_names_out(), top_n=5)
+
+top_words_list = []
+# 결과 출력
+for i, words in enumerate(top_words):
+    for word in words:
+        top_words_list.append(word)
+
+top_words_list = list(set(top_words_list))
