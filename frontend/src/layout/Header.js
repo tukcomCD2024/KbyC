e4224293f88d08323ac172b6c5a2dbcff59a97eb
groom2hub <groomwnsgur35@gmail.com>
2024-09-13 16:58:14 +0900

    Design: Header

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -20,14 +20,15 @@ const Header = () => {
 
     return (
         <header className='App-header'>
-            <button className='App-header-link-logo' onClick={() => handleNavigation('/')}>
-                <img src={logo} alt='home' className='App-header-link-logo-image' />
-            </button>
-            <button className='App-header-link' onClick={() => handleNavigation('/service/topictrends')}>토픽 트렌드</button>
-            <button className='App-header-link' onClick={() => handleNavigation('/service/realtimesearchwords')}> 실시간 검색어</button>
-            <button className='App-header-link' onClick={() => handleNavigation('/service/googletrends')}>트렌드 데이터</button>
-            <button className='App-header-link' onClick={() => handleNavigation('/board')}>게시판</button>
-        
+            <div className='App-header-menu-container'>
+                <button className='App-header-link-logo' onClick={() => handleNavigation('/')}>
+                    <img src={logo} alt='home' className='App-header-link-logo-image' />
+                </button>
+                <button className='App-header-link' onClick={() => handleNavigation('/service/topictrends')}>토픽 트렌드</button>
+                <button className='App-header-link' onClick={() => handleNavigation('/service/realtimesearchwords')}> 실시간 검색어</button>
+                <button className='App-header-link' onClick={() => handleNavigation('/service/googletrends')}>트렌드 데이터</button>
+                <button className='App-header-link' onClick={() => handleNavigation('/board')}>게시판</button>
+            </div>
             <form class="search-form" onSubmit={handleSubmit}>
                 <div className='search-container'>
                     <img src={logo2} alt="search icon" className="search-form-logo-image" />
