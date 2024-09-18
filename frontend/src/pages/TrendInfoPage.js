dae9cdf56cd05cd0bf9c6fae8fa032ef3641a2e1
Blu2ming <yeox84@naver.com>
2024-09-18 21:53:04 +0900

    Feat: Keywords

--- frontend/src/pages/TrendInfoPage.js
+++ frontend/src/pages/TrendInfoPage.js
@@ -231,6 +231,7 @@ const TrendInfoPage = () => {
             <div id='trend-reactions'>
             <p className='trendinfo-content-title'>연관 키워드</p>
             <p1 className='trendinfo-content-list'>- 네이버 블로그, 카페 키워드</p1>
+            {loading3 && <p className='trendinfo-content-list'>로딩 중...</p>}
             <div className='trendinfo-contents'>
               {wordList2.map((word, index) => (
                 <p key={index} className='trendinfo-content-list'>
